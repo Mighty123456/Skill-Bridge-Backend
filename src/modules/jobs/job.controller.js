18d49f2c4ec5527b7ab12dd47669828bc1bc156f
Ansh13032005 <anshparikh.1305@gmail.com>
2026-01-27 16:46:58 +0530

    b57

--- src/modules/jobs/job.controller.js
+++ src/modules/jobs/job.controller.js
@@ -3,6 +3,7 @@ const User = require('../users/user.model');
 const Worker = require('../workers/worker.model');
 const Notification = require('../notifications/notification.model');
 const { uploadOptimizedImage } = require('../../common/services/cloudinary.service');
+const { calculateDistance } = require('../../common/utils/geo');
 const logger = require('../../config/logger');
 
 // Create a new job
@@ -521,20 +522,3 @@ exports.confirmCompletion = async (req, res) => {
     }
 };
 
-// Helper: Calculate distance between two coordinates in km (Haversine formula)
-function calculateDistance(lat1, lon1, lat2, lon2) {
-    const R = 6371; // Radius of the earth in km
-    const dLat = deg2rad(lat2 - lat1);
-    const dLon = deg2rad(lon2 - lon1);
-    const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const d = R * c; // Distance in km
-    return d;
-}
-
-function deg2rad(deg) {
-    return deg * (Math.PI / 180);
-}
